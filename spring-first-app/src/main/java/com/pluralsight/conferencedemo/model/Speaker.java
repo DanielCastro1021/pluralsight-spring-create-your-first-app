aea9c07ec934666b9d44988bbfebd45f4b92dcf2
Daniel Castro <danielcastrofafe@gmail.com>
2022-06-21 10:01:37 +0100

    Added image variable to JPA Entity

--- spring-first-app/src/main/java/com/pluralsight/conferencedemo/model/Speaker.java
+++ spring-first-app/src/main/java/com/pluralsight/conferencedemo/model/Speaker.java
@@ -1,5 +1,7 @@
 package com.pluralsight.conferencedemo.model;
 
+import org.hibernate.annotations.Type;
+
 import javax.persistence.*;
 import java.util.List;
 
@@ -14,7 +16,9 @@ public class Speaker {
     private String title;
     private String company;
     private String speaker_bio;
-
+    @Lob
+    @Type(type = "org.hibernate.type.BinaryType")
+    private byte[] speaker_photo;
     @ManyToMany(mappedBy = "speakers")
     private List<Session> sessions;
 
@@ -75,5 +79,13 @@ public class Speaker {
 
     public void setSessions(List<Session> sessions) {
         this.sessions = sessions;
+    }
+
+    public byte[] getSpeaker_photo() {
+        return speaker_photo;
+    }
+
+    public void setSpeaker_photo(byte[] speaker_photo) {
+        this.speaker_photo = speaker_photo;
     }
 }
