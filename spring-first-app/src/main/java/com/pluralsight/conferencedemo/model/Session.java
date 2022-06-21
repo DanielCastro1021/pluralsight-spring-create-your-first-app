098a500992d334ac94571eb579dc7b20ce6b6474
Daniel Castro <danielcastrofafe@gmail.com>
2022-06-21 10:46:15 +0100

    Fixed Variable Type

--- spring-first-app/src/main/java/com/pluralsight/conferencedemo/model/Session.java
+++ spring-first-app/src/main/java/com/pluralsight/conferencedemo/model/Session.java
@@ -6,7 +6,7 @@ import javax.persistence.*;
 import java.util.List;
 
 @Entity(name = "sessions")
-@JsonIgnoreProperties({"hibernateLazyInitializer","handler"})
+@JsonIgnoreProperties({"hibernateLazyInitializer", "handler"})
 public class Session {
 
     @Id
@@ -14,11 +14,9 @@ public class Session {
     private Long session_id;
     private String session_name;
     private String session_description;
-    private String session_length;
+    private Long session_length;
     @ManyToMany
-    @JoinTable(name = "session_speakers",
-            joinColumns = @JoinColumn(name = "session_id"),
-            inverseJoinColumns = @JoinColumn(name = "speaker_id"))
+    @JoinTable(name = "session_speakers", joinColumns = @JoinColumn(name = "session_id"), inverseJoinColumns = @JoinColumn(name = "speaker_id"))
     private List<Speaker> speakers;
 
     public Session() {
@@ -48,11 +46,11 @@ public class Session {
         this.session_description = session_description;
     }
 
-    public String getSession_length() {
+    public Long getSession_length() {
         return session_length;
     }
 
-    public void setSession_length(String session_length) {
+    public void setSession_length(Long session_length) {
         this.session_length = session_length;
     }
 
